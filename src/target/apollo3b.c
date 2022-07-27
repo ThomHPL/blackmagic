9ad6b8629a7cfbe97e38833aeb32bac4677b291b
Thomas Herpoel <thomas.herpoel@gmail.com>
2022-07-28 01:48:27 +0200

    compiles OK

--- src/target/apollo3b.c
+++ src/target/apollo3b.c
@@ -4,6 +4,34 @@
 #include "target_internal.h"
 #include "adiv5.h"
 
+#define APOLLO3B_CHIPPN 0x40020000
+
+#define APOLLO3_FLASH_BASE 0x00000000
+#define APOLLO3_FLASH_SIZE 0x00100000
+#define APOLLO3_BLOCKSIZE 2048
+
+static int apollo3_flash_erase(){//struct target_flash *f, target_addr addr, size_t len) {
+    return 0;
+}
+static int apollo3_flash_write(){//struct target_flash *f, target_addr dest, const void *src, size_t len) {
+    return 0;
+}
+
+static void apollo3_add_flash(target *t)
+{
+    struct target_flash *f = calloc(1, sizeof(*f));
+    f->start = APOLLO3_FLASH_BASE;
+    f->length = APOLLO3_FLASH_SIZE;
+    f->blocksize = APOLLO3_BLOCKSIZE;
+    f->erase = apollo3_flash_erase;
+    f->write = apollo3_flash_write;
+    target_add_flash(t, f);
+}
+
 bool apollo3b_probe(target *const t) {
+    // uint32_t chipid;
+	// chipid = target_mem_read32(t, APOLLO3B_CHIPPN);
+    // DEBUG_INFO("Chip IP: %8X\n",chipid);
+    apollo3_add_flash(t);
     return true;
 }
